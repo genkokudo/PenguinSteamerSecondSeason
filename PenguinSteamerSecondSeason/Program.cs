d4a5da204f1a716e1b6dbd97d4167dda0d142e4e
ginpaydo <y-toyoda@live.jp>
2019-06-03 14:16:07 +0900

    ハロワ

--- PenguinSteamerSecondSeason/Program.cs
+++ PenguinSteamerSecondSeason/Program.cs
@@ -6,7 +6,7 @@ namespace PenguinSteamerSecondSeason
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Work!");
         }
     }
 }
