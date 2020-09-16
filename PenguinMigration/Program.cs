8e8bacac3d782f0eacbe19ad4b2e3acfa02ec828
Ginpay Iwatobi <y-toyoda@live.jp>
2020-09-16 23:52:23 +0900

    マイグレ用のプロジェクト追加

---
+++ PenguinMigration/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// マイグレーションするためだけのプロジェクトですぞ～
+/// プログラム実行したら移行されるようにする。
+/// </summary>
+namespace PenguinMigration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- Penguinium/Infrastructure/Database/AppDbContext.cs
+++ Penguinium/Infrastructure/Database/AppDbContext.cs
@@ -1,14 +1,24 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Penguinium.Entities;
 
+
 namespace Penguinium.Infrastructure.Database
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<Test> Tests { get; set; }
+
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
         { }
 
-        public DbSet<Test> Tests { get; set; }
+        // TODO:これ、ホスティングするからいらないっぽい
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            var connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;Integrated Security=true;Database=PenguinSteamer";  // local.settings.jsonとかと同じにすること
+            optionsBuilder.UseSqlServer(connectionString);
+        }
     }
 }
