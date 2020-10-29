87b7be742125ecfecf98821902ffe6487585cc65
Ginpay Iwatobi <y-toyoda@live.jp>
2020-10-30 00:09:45 +0900

    再コミット

--- BlizzardHaunt/Startup.cs
+++ BlizzardHaunt/Startup.cs
@@ -43,21 +43,14 @@ namespace BlizzardHaunt
 
         public void ConfigureServices(IServiceCollection services)
         {
-            // 環境変数から接続文字列とパスワードを取得する。
-            // SQLCONNSTR_BHで接続文字列を環境変数に設定。Azureのアプリケーション設定の接続文字列にも"BH"として設定すること。
-            var constr = Configuration.GetConnectionString("BH");
-            if (string.IsNullOrWhiteSpace(constr))
-            {
-                // 結局、EFCoreのToolは環境変数を読むことはできない（ASPNETCORE_ENVIRONMENTはappsettings.jsonを読む時に認識する）
-                // マイグレーションの生成の時用に、仮の接続文字列をハードコードかappsettings.Development.jsonに書くのが正解。
-                constr = Configuration.GetValue<string>("ConnectionStringForMigration");
-            }
-            // DBの設定
-            // クラスライブラリにDbContextを作成する場合、MigrationsAssemblyを指定する必要がある。
+            // 環境変数から接続文字列とパスワードを取得する。（TODO:本番系のみ）
+            //var constr = Configuration.GetConnectionString("BH");
+
+            // 設定ファイルから接続文字列とパスワードを取得する。（開発時のみ）
+            var constr = Configuration.GetValue<string>($"ConnectionStrings:DefaultConnection");
             services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(constr, b => b.MigrationsAssembly("BlizzardHaunt"))
-                );
-            
+            options.UseSqlServer(constr, b => b.MigrationsAssembly("BlizzardHaunt")));   // SQLCONNSTR_BH
+
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
